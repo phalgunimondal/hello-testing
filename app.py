765e8265841d8b81f6769492a48e0b8ce5e21ae5
Phalguni Mondal <phalguni.0208@gmail.com>
2023-04-27 17:44:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,23 +7,23 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-@app.route('/weather')
-def conv_weather_apt():
-    x = requests.get('https://api.openweathermap.org/data/2.5/weather?q=Kolkata,in&APPID=767ab06d4aae399b47d43e6ef88df834')
-    #print(x.status_code)
-    data = x.json()
-    #print(data)
-    # of the first matching location
-    latitude = data['coord']['lat']
-    longitude = data['coord']['lon']
-    temp = data['main']['temp']
-    pressure = data['main']['pressure']
-    humidity = data['main']['humidity']
-    wind_speed = data['wind']['speed']
-    my_json_string = json.dumps({'Lat': latitude, 'Long': longitude,'Temp':temp, 'Pres':pressure,'Hum':humidity,'Wind':wind_speed})
-    #print("Latitude:%s,Longitude:%s,Temp:%s,Pressure:%s,humidity:%s,Wind_speed:%s"%(latitude,longitude,temp,pressure,humidity,wind_speed))
-    #print(my_json_string)
-    return my_json_string
+# @app.route('/weather')
+# def conv_weather_apt():
+#     x = requests.get('https://api.openweathermap.org/data/2.5/weather?q=Kolkata,in&APPID=767ab06d4aae399b47d43e6ef88df834')
+#     #print(x.status_code)
+#     data = x.json()
+#     #print(data)
+#     # of the first matching location
+#     latitude = data['coord']['lat']
+#     longitude = data['coord']['lon']
+#     temp = data['main']['temp']
+#     pressure = data['main']['pressure']
+#     humidity = data['main']['humidity']
+#     wind_speed = data['wind']['speed']
+#     my_json_string = json.dumps({'Lat': latitude, 'Long': longitude,'Temp':temp, 'Pres':pressure,'Hum':humidity,'Wind':wind_speed})
+#     #print("Latitude:%s,Longitude:%s,Temp:%s,Pressure:%s,humidity:%s,Wind_speed:%s"%(latitude,longitude,temp,pressure,humidity,wind_speed))
+#     #print(my_json_string)
+#     return my_json_string
 
 @app.route('/airpollution')
 def air_pollution():
